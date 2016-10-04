974e2f2c98e4a19c0e9cab397a0997b829071290
slbing <slbing@maplecloudy.com>
2016-10-04 16:36:59 +0800

    rm indexer pom

--- indexer/src/main/java/com/maplecloudy/indexer/solr/CustomCoreDescriptor.java
+++ indexer/src/main/java/com/maplecloudy/indexer/solr/CustomCoreDescriptor.java
@@ -1,4 +1,3 @@
-
 package com.maplecloudy.indexer.solr;
 
 import java.util.Properties;
@@ -7,38 +6,36 @@ import org.apache.solr.core.CoreContainer;
 import org.apache.solr.core.CoreDescriptor;
 
 public class CustomCoreDescriptor extends CoreDescriptor {
-
-	Properties coreProperties;
-	final CoreContainer coreContainer;
-
-	public CustomCoreDescriptor(CoreContainer coreContainer, String name,
-			String instanceDir) {
-		super(coreContainer, name, instanceDir);
-		this.coreContainer = coreContainer;
-	}
-
-	public Properties getCoreProperties() {
-		return coreProperties;
-	}
-
-	public void setCoreProperties(Properties coreProperties) {
-		if (this.coreProperties == null) {
-			Properties p = initImplicitProperties();
-			this.coreProperties = new Properties(p);
-			if (coreProperties != null)
-				this.coreProperties.putAll(coreProperties);
-		}
-	}
-
-	Properties initImplicitProperties() {
-		Properties implicitProperties = new Properties(
-				coreContainer.getContainerProperties());
-		implicitProperties.setProperty("solr.core.name", name);
-		implicitProperties.setProperty("solr.core.instanceDir", instanceDir);
-		implicitProperties.setProperty("solr.core.dataDir", getDataDir());
-		implicitProperties.setProperty("solr.core.configName", configName);
-		implicitProperties.setProperty("solr.core.schemaName", schemaName);
-		return implicitProperties;
-	}
-
+  
+  Properties coreProperties;
+  final CoreContainer coreContainer;
+  public CustomCoreDescriptor(CoreContainer coreContainer, String name,
+      String instanceDir) {
+    super(coreContainer, name, instanceDir);
+    this.coreContainer = coreContainer;
+    this.name = name;
+    this.instanceDir = instanceDir;
+  }
+  
+  public Properties getCoreProperties() {
+    return coreProperties;
+  }
+  
+  public void setCoreProperties(Properties coreProperties) {
+    if (this.coreProperties == null) {
+      Properties p = initImplicitProperties();
+      if (coreProperties == null) this.coreProperties = new Properties(p);
+      this.coreProperties.putAll(coreProperties);
+    }
+  }
+  
+  Properties initImplicitProperties() {
+    Properties implicitProperties = new Properties(
+        coreContainer.getContainerProperties());
+    implicitProperties.setProperty("solr.core.name", name);
+    implicitProperties.setProperty("solr.core.instanceDir", instanceDir);
+    implicitProperties.setProperty("solr.core.dataDir", getDataDir());
+    return implicitProperties;
+  }
+  
 }
