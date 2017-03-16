be9362e1d3b2b45515dcd98362884631f59aeb76
slbing <slbing@maplecloudy.com>
2017-03-16 01:23:01 -0700

    mdoify hadoop verion to 5.8.3

--- distribute-job-engin/build.sh
+++ distribute-job-engin/build.sh
@@ -1,2 +1,4 @@
 apache-maven-3.3.9.zip/apache-maven-3.3.9/bin/mvn clean package -f sixddefense.zip/sixddefense/pom.xml
-hadoop fs -put sixddefense.zip/sixddefense/target/sixddefense-dist-bin/ 
+echo "start deploy the project....."
+hadoop fs -copyFromLocal -f sixddefense.zip/sixddefense/sixddfense-dist/target/sixddefense-dist-1.0.0-SNAPSHOT-bin sixddefense-dist-1.0.0-SNAPSHOT-bin
+echo "succes to deploy the project....."
