1c71eb6efd53a655331ec7060b3f124888f8a3be
xtq <xutq@maplecloudy.com>
2019-01-12 11:55:45 +0800

    httputils

--- spider/src/main/java/com/maplecloudy/spider/protocol/httpmethod/HttpUtils.java
+++ spider/src/main/java/com/maplecloudy/spider/protocol/httpmethod/HttpUtils.java
@@ -9,12 +9,16 @@ import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
